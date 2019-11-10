470b9dc78fe8451ecb6f5e25370c35dd8887f019
jhurlburt <jim.hurlburt@gmail.com>
2019-11-10 16:17:44 -0700

    commit

--- src/helpers/Helper.ts
+++ src/helpers/Helper.ts
@@ -11,40 +11,58 @@ export class Helper {
     };  
   
     static collideWithBox(char: BoundingBox, objects: BoundingBox[], v: number = 0, s: number = 0) {
-      var hasCollidedAny = false, 
+      let hasCollidedAny = false, 
         hasCollidedX = false, 
         hasCollidedY = false;
       
       if (char != null) {
-        var orig_top = char.boundingBox.y - char.boundingBox.frameHeight,
+        let orig_top = char.boundingBox.y - char.boundingBox.frameHeight,
           orig_bot = char.boundingBox.y,
           orig_rt = char.boundingBox.x + char.boundingBox.frameWidth,
           orig_lt = char.boundingBox.x;
 
-        var dest_top = orig_top+v,
+        let dest_top = orig_top+v,
           dest_bot = orig_bot+v,
           dest_rt = orig_rt+s,
           dest_lt = orig_lt+s;
 
-        var isCollidedTop = false, willCollideTop = false,
+        let isCollidedTop = false, willCollideTop = false,
           isCollidedBot = false, willCollideBot = false,
           isCollidedLt = false, willCollideLt = false,
-          isCollidedRt = false, willCollideRt = false;
+          isCollidedRt = false, willCollideRt = false,
+          isCollided = false;
           
         objects.forEach(element => {
           element.resetCollided();
   
           if (!hasCollidedAny){
-            var block_top = element.boundingBox.y - element.boundingBox.frameHeight,
+            let block_top = element.boundingBox.y - element.boundingBox.frameHeight,
               block_bot = element.boundingBox.y,
               block_rt = element.boundingBox.x + element.boundingBox.frameWidth,
               block_lt = element.boundingBox.x;
 
-            isCollidedTop = (orig_bot>=block_top);
-            isCollidedBot = (orig_top<=block_bot);
-            isCollidedRt = (orig_lt<=block_rt);
-            isCollidedLt = (orig_rt>=block_lt);
-  
+            isCollided = ((orig_bot>=block_top) && (orig_bot<=block_bot)) ||
+              ((orig_top<=block_bot) && (orig_top>=block_top)) ||
+              ((orig_rt>=block_lt) && (orig_rt<=block_rt)) ||
+              ((orig_lt<=block_rt) && (orig_lt>=block_lt));
+            
+            isCollidedTop = (orig_bot>=block_top) && (orig_top<=block_top) && ((orig_rt>=block_lt) || (orig_lt<=block_rt));
+            isCollidedBot = (orig_top<=block_bot) && (orig_bot>=block_bot) && ((orig_rt>=block_lt) || (orig_lt<=block_rt));
+            isCollidedRt = (orig_lt<=block_rt) && (orig_rt>=block_rt);
+            isCollidedLt = (orig_rt>=block_lt) && (orig_lt<=block_lt);
+
+            willCollideTop = (dest_bot>=block_top) && (dest_bot<=block_top) && (dest_rt>=block_lt) && (dest_lt<=block_rt);
+            willCollideBot = (dest_top<=block_bot) && (dest_bot>=block_bot) && (dest_rt>=block_lt) && (dest_lt<=block_rt);
+            willCollideRt = (dest_lt<=block_rt) && (dest_rt>=block_rt) && (dest_top<=block_bot) && (dest_bot>=block_top);
+            willCollideLt = (dest_rt>=block_lt) && (dest_lt<=block_lt) && (dest_top<=block_bot) && (dest_bot>=block_top);
+            
+            element.hasCollidedTop    = !isCollidedTop && willCollideTop;
+            element.hasCollidedBottom = !isCollidedBot && willCollideBot;
+            element.hasCollidedLeft   = !isCollidedLt && willCollideLt;
+            element.hasCollidedRight  = !isCollidedRt && willCollideRt;
+
+            hasCollidedAny = (element.hasCollidedTop || element.hasCollidedBottom) || (element.hasCollidedLeft || element.hasCollidedRight);            
+
             //   if ((orig_top< block_top) && (orig_top< block_bot) && (orig_bot>=block_top) && (orig_bot<=block_bot)) {
             //   isCollidedTop = true;
             //   console.log("orig collided: 1200"); 
@@ -62,11 +80,6 @@ export class Helper {
             //   console.log("orig collided: 0900"); 
             // }
 
-            willCollideTop = (dest_bot>=block_top);
-            willCollideBot = (dest_top>=block_top);
-            willCollideRt = (dest_lt<=block_rt);
-            willCollideLt = (dest_rt>=block_lt);
-
             // if ((dest_top< block_top) && (dest_top< block_bot) && (dest_bot>=block_top) && (dest_bot<=block_bot)) {
             //   willCollideTop = true;
             //   console.log("dest collided: 0000"); 
@@ -83,12 +96,6 @@ export class Helper {
             //   willCollideLt = true;
             //   console.log("dest collided: 0900"); 
             // }
-            element.hasCollidedTop    = (isCollidedLt && isCollidedRt) && (willCollideTop && willCollideTop);
-            element.hasCollidedBottom = (isCollidedLt && isCollidedRt) && willCollideBot;
-            element.hasCollidedLeft   = (isCollidedTop && isCollidedBot) && willCollideLt;
-            element.hasCollidedRight  = (isCollidedTop && isCollidedBot) && willCollideRt;
-
-            hasCollidedAny = (element.hasCollidedTop || element.hasCollidedBottom) || (element.hasCollidedLeft || element.hasCollidedRight);            
           } 
         });
       }
